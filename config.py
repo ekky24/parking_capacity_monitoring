8cdfc2c79b8eac216fa1c2d1968e23add5ba347a
Cloud User <rocky@ip-172-31-25-171.ap-southeast-3.compute.internal>
2024-08-01 06:23:58 +0700

    continue dev

--- config.py
+++ config.py
@@ -2,28 +2,36 @@ from shapely.geometry import Polygon
 
 LOCATION_CONF = {
     'sol': {
-        'host': '103.167.31.202:554/H.264',
+        'host': '182.0.23.170:554/ch1/main',
         'username': 'admin',
-        'password': 'KGMJLP',
+        'password': 'FNZLLZ',
         'area': 'sol',
-        'max_capacity': [15],
+        'max_capacity': [15, 15, 15],
         'region': [
             {
                 "name": "sol_1",
-                "polygon": Polygon([(21, 80), (21, 665), (1240, 665), (1240, 80)]),  # Polygon points
+                "polygon": Polygon([(12, 381), (3, 471), (124, 508), (165, 403)]),  # Polygon points
                 "counts": 0,
                 "draggin": False,
                 "region_color": (37, 255, 255), # BGR value
                 "text_color": (0, 0, 0) # Region text color
             },
-            # {
-            #     "name": "sol_2",
-            #     "polygon": Polygon([(460, 100), (600, 100), (665, 458), (417, 557)]),  # Polygon points
-            #     "counts": 0,
-            #     "draggin": False,
-            #     "region_color": (37, 255, 255), # BGR value
-            #     "text_color": (0, 0, 0) # Region text color
-            # }
+            {
+                "name": "sol_2",
+                "polygon": Polygon([(439, 222), (384, 349), (552, 347), (540, 212)]),  # Polygon points
+                "counts": 0,
+                "draggin": False,
+                "region_color": (37, 255, 255), # BGR value
+                "text_color": (0, 0, 0) # Region text color
+            },
+            {
+                "name": "sol_3",
+                "polygon": Polygon([(858, 218), (759, 241), (1258, 501), (1266, 367)]),  # Polygon points
+                "counts": 0,
+                "draggin": False,
+                "region_color": (37, 255, 255), # BGR value
+                "text_color": (0, 0, 0) # Region text color
+            },
         ]
     }
 }
--- predict.sh
+++ predict.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python "yolo_app_park.py" --source "rtsp" --save-img --device 0 --weights "yolov8m.pt" --classes 2 --area $CCTV_AREA
+python "yolo_app_park.py" --source "rtsp" --save-img --device 0 --weights "yolov8l.pt" --classes 2 --area $CCTV_AREA
