46bfbac187d77009dfd97740ef7989090fa3444b
Ekky Rino <ekky.rhino@gmail.com>
2024-08-01 12:38:11 +0700

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -2,5 +2,6 @@
 NV="v0.6"
 docker build -t parking_monitoring:$NV .
 docker rm parking_monitoring
-docker run --log-opt max-size=10m --log-opt max-file=3 -e CCTV_AREA="sol" --name parking_monitoring -v /mnt/data/machine_learning/output:/app/output --gpus all -d --restart unless-stopped parking_monitoring:$NV
-# docker run --log-opt max-size=10m --log-opt max-file=3 -e CCTV_AREA="sol" --name parking_monitoring -v /mnt/data/machine_learning/output:/app/output --gpus all --rm -it parking_monitoring:$NV /bin/bash
+
+# Tahu Sumedang
+docker run --log-opt max-size=10m --log-opt max-file=3 -e CCTV_AREA="tsu" --name parking_monitoring -v /mnt/data/machine_learning/output:/app/output --gpus all -d --restart unless-stopped parking_monitoring:$NV
