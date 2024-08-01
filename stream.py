34c8fb7bf696028d10a14b043d7e4ab5360b0858
Ekky Rino <ekky.rhino@gmail.com>
2024-08-01 08:13:27 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,6 +1,11 @@
 import cv2
 
-cap = cv2.VideoCapture('rtsp://admin:KGMJLP@103.167.31.202:554/H.264')
+# cap = cv2.VideoCapture('rtsp://admin:KGMJLP@103.167.31.202:554/H.264')
+# cap = cv2.VideoCapture('rtsp://admin:IMGTDW@182.0.21.152:554/ch1/main')
+# cap = cv2.VideoCapture('rtsp://admin:IMGTDW@182.0.21.152:554/ch2/main')
+cap = cv2.VideoCapture('rtsp://admin:FNZLLZ@182.0.23.170:554/ch2/main')
+
+frame_w, frame_h = 1280, 720
 
 if not cap.isOpened():
     print("Error: Could not open video stream")
@@ -13,7 +18,8 @@ while True:
     if not ret:
         print("Error: Failed to capture image")
         break
-
+    
+    frame = cv2.resize(frame, (frame_w, frame_h))
     # Tampilkan frame
     cv2.imshow('RTSP Stream', frame)
 
