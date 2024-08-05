f49c6f57bde34a22444af8c5f2c5d13d31875176
Ekky Rino <ekky.rhino@gmail.com>
2024-08-05 12:23:50 +0700

    Create rm_old_video.sh

---
+++ rm_old_video.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Format: yyyymmdd ex: 20240803
+RM_DATE="20240802"
+
+rm -rf /mnt/data/machine_learning/output/parking_monitoring/masjid/$RM_DATE
+rm -rf /mnt/data/machine_learning/output/parking_monitoring/cig/$RM_DATE
+rm -rf /mnt/data/machine_learning/output/parking_monitoring/spklu/$RM_DATE
+rm -rf /mnt/data/machine_learning/output/parking_monitoring/mcd/$RM_DATE
+rm -rf /mnt/data/machine_learning/output/parking_monitoring/tsu/$RM_DATE
+rm -rf /mnt/data/machine_learning/output/parking_monitoring/sol/$RM_DATE
+rm -rf /mnt/data/machine_learning/output/parking_monitoring/stb/$RM_DATE
