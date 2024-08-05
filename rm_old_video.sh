152916cebcbcf7e2819d9618f88b27c932b61d34
Ekky Rino <ekky.rhino@gmail.com>
2024-08-06 06:45:38 +0700

    Update rm_old_video.sh

--- rm_old_video.sh
+++ rm_old_video.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Format: yyyymmdd ex: 20240803
-RM_DATE="20240802"
+RM_DATE="20240803"
 
 rm -rf /mnt/data/machine_learning/output/parking_monitoring/masjid/$RM_DATE
 rm -rf /mnt/data/machine_learning/output/parking_monitoring/cig/$RM_DATE
