29f34f8b9eca467ea7bac6ce84ec5a1062e82b39
Ekky Rino <ekky.rhino@gmail.com>
2024-08-09 16:26:28 +0700

    adding new classes

--- predict.sh
+++ predict.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python "dist/yolo_app_park.py" --source "rtsp" --save-img --device 0 --weights "yolov8l.pt" --classes 2 --area $CCTV_AREA
+python "dist/yolo_app_park.py" --source "rtsp" --save-img --device 0 --weights "yolov8l.pt" --classes 2 7 --area $CCTV_AREA
