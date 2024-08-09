ae463a0982b392c5ee6ee13a903d9d9f80262fdb
Ekky Rino <ekky.rhino@gmail.com>
2024-08-09 19:33:32 +0700

    Update yolo_app_park.py

--- yolo_app_park.py
+++ yolo_app_park.py
@@ -108,7 +108,7 @@ def run(
 
     # Video Setup
     VideoCapture = cv2.VideoCapture(source, cv2.CAP_FFMPEG)
-    VideoCapture.set(cv2.CAP_PROP_FPS, 5)
+    VideoCapture.set(cv2.CAP_PROP_FPS, 12)
     frame_w, frame_h, fps = (int(VideoCapture.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, 
                                                                 cv2.CAP_PROP_FRAME_HEIGHT, 
                                                                 cv2.CAP_PROP_FPS
